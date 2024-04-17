a4e77a45ca8e1d706af13e4c88d46aa686bd6d1f
Rikie Patrick <rikiepatrick@gmail.com>
2024-04-17 13:58:14 +0100

    Refactor game logic and player order in Game.py

--- lib/Game.py
+++ lib/Game.py
@@ -18,21 +18,47 @@ class Game:
         self.game_over = False
         self.trick: list[tuple[Player, Card]] = []
         self.tricks = []
+        self.previous_winner: None | Player = None
 
     def start_game(self):
+
+        random.shuffle(self.players)
+
         while not self.game_over:
-            clear()
             self.print_scores()
             self.trick = []
 
-            for player in self.players[::-1]:
-                card_played = self.play_card(player)
-                self.trick.append((player, card_played))
-                sleep(0.5)
+            self.set_starting_player()
 
-            self.check_trick_winner(card_played)
+            for i, player in enumerate(self.players):
+                card_played = None
+                if len(self.tricks) == 0 and i == 0:
+                    for j, card in enumerate(player.hand):
+                        if card == Card(2, "Clubs"):
+                            card_played = player.play_card(j)
+                            break
+                else:
+                    card_played = self.play_card(player)
+                self.trick.append((player, card_played))
+
+            self.previous_winner = self.check_trick_winner(self.trick)
             self.tricks.append(self.trick)
             self.check_game_over()
+
+    def set_starting_player(self):
+        for player in self.players:
+
+            has_2_clubs = Card(2, "Clubs") in player.hand
+            is_prev_winner = self.previous_winner and player == self.previous_winner
+
+            if has_2_clubs or is_prev_winner:
+                self.players.remove(player)
+                self.players.insert(0, player)
+                if has_2_clubs:
+                    print(player, " has 2 of clubs and will start the game")
+                if is_prev_winner:
+                    print(player, "won the last round and will lead")
+                break
 
     def play_card(self, player: Player):
         hand = player.hand
@@ -69,31 +95,57 @@ class Game:
 
         return player.play_card(card_played_idx)
 
-    def check_trick_winner(self, trick):
-        winning_player = None
-        max_card = -1
+    def check_trick_winner(self, trick: list[tuple[Player, Card]]):
+        [winning_player, winning_card] = trick[0]
+        lead_suit = winning_card.suit
         for player, card in self.trick:
-            if card.value > max_card:
-                max_card = card.value
+            if card.suit == lead_suit and card.value > winning_card.value:
+                winning_card = card
                 winning_player = player
 
         print(
             f"{'You' if winning_player.id == 1 else winning_player.name} won this hand"
         )
         winning_player.score += 1
-        sleep(3)
+        return winning_player
 
     def print_scores(self):
+        print("\n" + "Scores on the doors...")
         for player in self.players:
-            print(f"{player.name} scored: {player.score}")
+
+            print(player.score, "-", player.name)
+        print("\n")
 
     def check_game_over(self):
         if len(self.player1.hand) == 0:
             self.game_over = True
 
-        print(
-            """------------
+            print(
+                """------------
 Game Over
 ------------"""
-        )
-        self.print_scores()
+            )
+            winners = self.check_winner()
+            if len(winners) == 1:
+                print("THE WINNER IS .....")
+                sleep(5)
+                print(winners[0].name.upper())
+            else:
+                names = [winner.name.upper() for winner in winners]
+                print("THE WINNERS ARE .....")
+                sleep(5)
+                print(f"{', '.join(names[:-1])} and {names[-1]}")
+
+            self.print_scores()
+
+    def check_winner(self):
+        max_score = -1
+        winners: list[Player] = []
+        for player in self.players:
+            if player.score > max_score:
+                max_score = player.score
+                winners = [player]
+                continue
+            if player.score == max_score:
+                winners.append(player)
+        return winners
